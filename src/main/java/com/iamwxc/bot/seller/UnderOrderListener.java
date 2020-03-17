17631fe8c7fddea44416cbd91c5f9b163d0f2119
Eddy Wei <anlowee@outlook.com>
2020-03-17 20:54:59 +0800

    :construction:
    add database connection

---
+++ src/main/java/com/iamwxc/bot/seller/UnderOrderListener.java
@@ -0,0 +1,35 @@
+package com.iamwxc.bot.seller;
+
+import com.forte.qqrobot.anno.Filter;
+import com.forte.qqrobot.anno.Listen;
+import com.forte.qqrobot.anno.depend.Beans;
+import com.forte.qqrobot.beans.messages.msgget.PrivateMsg;
+import com.forte.qqrobot.beans.messages.types.MsgGetTypes;
+import com.forte.qqrobot.sender.MsgSender;
+
+/**
+ * Class description goes here.
+ * <p>
+ * If you see this sentence, nothing ambiguous.
+ * </p>
+ *
+ * @author CC
+ * @version 1.0
+ */
+@Beans
+public class UnderOrderListener {
+
+    @Listen(MsgGetTypes.privateMsg)
+    @Filter("buy.*")
+    public void underOrderListen(PrivateMsg msg, MsgSender sender) {
+        String goodsName = msg.getMsg();
+        goodsName = goodsName.split("buy")[1];
+        if (goodsName == null || goodsName.equals("")) {
+            sender.SENDER.sendPrivateMsg(msg.getQQ(), "请输入想要的东西名称喵~");
+            return;
+        }
+        //TODO
+        // find good
+    }
+
+}
